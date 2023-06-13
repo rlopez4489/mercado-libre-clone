b1cf57b0ac223f52760ed8a9a0522232fe7199b9
rube4489 <ruben.lopez@doc24.com.ar>
2023-06-13 20:29:40 -0300

    Agrego effect para resetear value

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import Logo from "./Logo";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
   const [value, setValue] = useState("");
   const navigate = useNavigate();
 
+  useEffect(() => {
+    setValue(
+      location.pathname === "/" ? "" : sessionStorage.getItem("searchValue")
+    );
+    document.title = `Mercado Libre Argentina - Envíos Gratis en el día`;
+  }, [location.pathname]);
+
   const onSubmit = async () => {
+    sessionStorage.setItem("searchValue", value);
     navigate(`items?search=${value}`);
   };
 
