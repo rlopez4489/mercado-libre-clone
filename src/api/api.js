9a47ffab0cf8544cd269a56862c9f9e1ca5910e5
rube4489 <ruben.lopez@doc24.com.ar>
2023-06-13 03:12:00 -0300

    Agrego endpoints

---
+++ src/api/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API_URL = "https://api.mercadolibre.com";
+
+export const searchItems = async (query) => {
+  try {
+    const response = await axios.get(`${API_URL}/sites/MLA/search`, {
+      params: {
+        q: query,
+        limit: 4,
+      },
+    });
+    return response?.data?.results;
+  } catch (error) {
+    console.error("Error al obtener los resultados de búsqueda:", error);
+    throw error;
+  }
+};
+
+export const getItemDescription = async (itemId) => {
+  try {
+    const response = await axios.get(`${API_URL}/items/${itemId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener la descripción del artículo:", error);
+    throw error;
+  }
+};
+
+export default { searchItems, getItemDescription };
